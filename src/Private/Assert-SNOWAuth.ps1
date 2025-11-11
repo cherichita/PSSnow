59de6b0ab97599f12dd7c60ad9ba68510f88d904
Dimiter Todorov <dimiter@todorov.ca>
2025-11-11 11:06:36 -0500

    Assert-SNOWAuth - Use client_credentials grant_type when no refresh token is available and clientsecret is available. Use password grant_type when Credentials are available.

--- src/Private/Assert-SNOWAuth.ps1
+++ src/Private/Assert-SNOWAuth.ps1
@@ -18,14 +18,31 @@ function Assert-SNOWAuth() {
             $ProxyAuth = $script:SNOWAuth.ProxyAuth
             #? Get a new token
             $Body = @{
-                grant_type="refresh_token"
                 client_id = $script:SNOWAuth.ClientID
-                refresh_token = $script:SNOWAuth.token.refresh_token
+            }
+            if($Script:SNOWAuth.token.refresh_token){
+                $Body += @{
+                    grant_type="refresh_token"
+                    refresh_token = $script:SNOWAuth.token.refresh_token
+                }
+            }elseif($Script:SNOWAuth.Credential){
+                $Body += @{
+                    grant_type="password"
+                    username = $script:SNOWAuth.Credential.GetNetworkCredential().Username
+                    password = $script:SNOWAuth.Credential.GetNetworkCredential().Password
+                }
+            }elseif($script:SNOWAuth.ClientSecret){
+                $Body += @{
+                    grant_type="client_credentials"
+                }
+            }else{
+                Write-Error "No valid authentication method found to obtain OAuth token. One of Credential, Refresh Token, or Client Secret must be available." -ErrorAction Stop
             }
             # If client secret is provided, add it to the body. Public clients can refresh tokens without a client secret.
             if ($script:SNOWAuth.ClientSecret) {
                 $Body.client_secret = [System.Net.NetworkCredential]::new('dummy', $script:SNOWAuth.ClientSecret).Password
             }
+            Write-Verbose "Requesting new OAuth token from ServiceNow instance $($Script:SNOWAuth.Instance) grant_type: $($Body.grant_type)"
             $Token = Invoke-RestMethod -Method POST -uri "https://$($Script:SNOWAuth.Instance).service-now.com/oauth_token.do" -Body $Body -Verbose:$false @ProxyAuth
 
             $script:SNOWAuth.token = $token
